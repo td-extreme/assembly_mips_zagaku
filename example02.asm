0533b951b9eacaeedaa35c13a2c822585792c1bc
Tyler Decker <tyler.decker@gmail.com>
2016-08-03 20:45:12 -0500

    Update example02.asm

--- example02.asm
+++ example02.asm
@@ -1,16 +1,17 @@
 # Tyler Decker
-# 
+# 3 aug 2016
+# example to show how system calls work
 
 
 .text
 
-li $v0, 5
+li    $v0, 5          # system call to input int
 syscall
 
-add $a0, $v0, $v0
+add   $a0, $v0, $v0   # $a0 = $v0 + $v0
 
-li $v0, 1
+li    $v0, 1          # system call to print int
 syscall
 
-li $v0, 10
+li    $v0, 10         # system call to end
 syscall
